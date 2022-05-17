3e0b729e44e6322b8696ed661abcaf66995ded13
benshidanny11 <benshidanny11@gmail.com>
2022-05-17 13:10:10 +0200

    Implement video challenge

---
+++ README.md
@@ -0,0 +1,4 @@
+# video_challege
+>Live demo
+
+- Download app [here](https://dotpharma.rw/app/videochallege.apk)
---
+++ lib/main.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Video challenge',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+ bool isLoadingComments = false;
+  bool isCommentsShown = false;
+  String viewCommentsLabel = "View Comments";
+
+  //Youtube variables declarations
+
+  YoutubePlayerController? _controller;
+  TextEditingController? _idController;
+  TextEditingController? _seekToController;
+
+  PlayerState? _playerState;
+  YoutubeMetaData? _videoMetaData;
+  double _volume = 100;
+  bool _muted = false;
+  bool _isPlayerReady = false;
+
+    @override
+  void initState() {
+    super.initState();
+    _controller = YoutubePlayerController(
+      initialVideoId: YoutubePlayer.convertUrlToId("https://www.youtube.com/watch?v=dYZGnPuWmfQ").toString(),
+      flags: const YoutubePlayerFlags(
+        mute: false,
+        autoPlay: true,
+        disableDragSeek: false,
+        loop: false,
+        isLive: false,
+        forceHD: false,
+        enableCaption: true,
+      ),
+    )..addListener(listener);
+    _idController = TextEditingController();
+    _seekToController = TextEditingController();
+    _videoMetaData = const YoutubeMetaData();
+    _playerState = PlayerState.unknown;
+  }
+
+
+
+
+  void listener() {
+    if (_isPlayerReady && mounted && !_controller!.value.isFullScreen) {
+      setState(() {
+        _playerState = _controller!.value.playerState;
+        _videoMetaData = _controller!.metadata;
+      });
+    }
+  }
+  
+   @override
+  void deactivate() {
+    // Pauses video while navigating to next page.
+    _controller!.pause();
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    _controller!.dispose();
+    _idController!.dispose();
+    _seekToController!.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return YoutubePlayerBuilder(
+      player: YoutubePlayer(
+        controller: _controller as YoutubePlayerController,
+        showVideoProgressIndicator: true,
+        progressIndicatorColor: Colors.blueAccent,
+        topActions: <Widget>[
+          const SizedBox(width: 8.0),
+          Expanded(
+            child: Text(
+              _controller!.metadata.title,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+              ),
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+            ),
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.settings,
+              color: Colors.white,
+              size: 25.0,
+            ),
+            onPressed: () {},
+          ),
+        ],
+        onReady: () {
+          _isPlayerReady = true;
+        },
+      ),
+      builder: (contex, player) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Video challenge'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                  height: 250,
+                  width: double.infinity,
+                  child: player)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
